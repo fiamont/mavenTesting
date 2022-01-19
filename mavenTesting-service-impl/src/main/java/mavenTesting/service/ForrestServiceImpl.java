d0e4ae97a26638c176e4122fa5191496bf531f68
Sofia <sofia.montgomery@gmail.com>
2022-01-19 23:09:36 +0100

    added interface ForrestService and edited pom-files

--- mavenTesting-service-impl/src/main/java/mavenTesting/service/ForrestServiceImpl.java
+++ mavenTesting-service-impl/src/main/java/mavenTesting/service/ForrestServiceImpl.java
@@ -4,14 +4,16 @@ import mavenTesting.domain.Owl;
 import mavenTesting.domain.Pinetree;
 import mavenTesting.domain.Squirrel;
 
-public class ForrestServiceImpl {
+public class ForrestServiceImpl implements ForrestService{
 
     public Owl createOwl(int wingSpan, boolean hungry, int age, int weight, String name){
         return new Owl(name);
     }
+    @Override
     public Squirrel createSquirrel(String name){
         return new Squirrel(name);
     }
+    @Override
     public Pinetree createPinetree(int age){
         return new Pinetree(age);
     }
---
+++ mavenTesting-service/src/main/java/mavenTesting/service/ForrestService.java
@@ -0,0 +1,11 @@
+package mavenTesting.service;
+
+import mavenTesting.domain.Owl;
+import mavenTesting.domain.Pinetree;
+import mavenTesting.domain.Squirrel;
+
+public interface ForrestService {
+    public Owl createOwl(int wingSpan, boolean hungry, int age, int weight, String name);
+    public Squirrel createSquirrel(String name);
+    public Pinetree createPinetree(int age);
+}
