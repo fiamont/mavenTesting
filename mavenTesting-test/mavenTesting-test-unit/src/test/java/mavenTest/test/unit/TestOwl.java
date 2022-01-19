499519a65b6ae104fad5a96cbfaffb6e8da42919
Sofia <sofia.montgomery@gmail.com>
2022-01-19 17:25:57 +0100

    edited pom-files and class TestOwl

--- mavenTesting-test/mavenTesting-test-unit/src/test/java/mavenTest/test/unit/TestOwl.java
+++ mavenTesting-test/mavenTesting-test-unit/src/test/java/mavenTest/test/unit/TestOwl.java
@@ -1,9 +1,12 @@
 package mavenTest.test.unit;
 
+import mavenTesting.domain.Owl;
 import org.junit.Test;
 
 public class TestOwl {
 
     @Test
-    public void
+    public void owlConstructorTest(){
+        Owl owl;
+    }
 }
